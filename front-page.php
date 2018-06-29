9dc955e236999df97a882704aaf0278581009f1c
fahim shahreer <fahim.shahreer95@gmail.com>
2018-06-29 13:57:40 -0400

    Update front-page.php

--- front-page.php
+++ front-page.php
@@ -250,7 +250,7 @@
 
     <div class="contactus-bottom">
       <div class="contactus-bottom-container">
-          <?php echo do_shortcode('[contact-form-7 id="6" title="Contact form 1"]') ?>
+          <?php echo do_shortcode('[contact-form-7 id="5" title="Contact form 1"]') ?>
       </div>
     </div>
   </div>
